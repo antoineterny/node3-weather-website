f95b533955e3e303c7a82f6900fde76b333350f9
Antoine Terny <antoine.terny@gmail.com>
2021-03-03 22:56:04 +0100

    functionality added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,7 @@ const request = require("postman-request")
 
 const forecast = (long, lat, callback) => {
 	const url = `http://api.weatherstack.com/current?access_key=ab2f7177b17b1a8140ca9b1f1bc81c32&query=${lat},${long}`
+	// const url = `http://api.weatherstack.com/current?access_key=ab2f7177b17b1a8140ca9b1f1bc81c32&query=40,40`
 
 	request({ url: url, json: true }, (error, { body }) => {
 		if (error) {
@@ -15,7 +16,7 @@ const forecast = (long, lat, callback) => {
 			callback(
 				undefined,
 				body.current.weather_descriptions[0] +
-					`. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike}`
+					`. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike}. The humidity is ${body.current.humidity}% and the cloud cover ${body.current.cloudcover}%.`
 			)
 		}
 	})
